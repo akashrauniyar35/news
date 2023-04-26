be2dc6cc1f5366e40ee7f2a3286329cd45ce0033
Akash Rouniyar <akashrouniyar@192-168-1-114.tpgi.com.au>
2023-04-26 13:43:45 +1000

    project created and git first push

--- src/components/RightContent.js
+++ src/components/RightContent.js
@@ -9,7 +9,7 @@ function RightContent({data}) {
         {/* Top Content */
         }
         <div className='rightcontent_top'>
-            <img src="https://media.cnn.com/api/v1/images/stellar/prod/230420175947-joe-biden-041923.jpg?c=16x9&q=h_720,w_1280,c_fill"/>
+            <img src={data.articles[1].image}/>
             <h1>{data.articles[1].head}</h1>
             <p>{data.articles[1].byline.text}</p>
         </div>
